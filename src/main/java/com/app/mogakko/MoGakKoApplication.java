24101c496fe5b13df4894fdabcb08e67d31cdbbd
ZhenxiKIm <8055kjh@gmail.com>
2022-09-25 22:36:27 +0900

    Initial commit

---
+++ src/main/java/com/app/mogakko/MoGakKoApplication.java
@@ -0,0 +1,13 @@
+package com.app.mogakko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoGakKoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoGakKoApplication.class, args);
+    }
+
+}
